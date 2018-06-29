c6559b419c15e5f4777f6b10784a53e54ed0075f
Robert Jankowski <37384654+robertjankowski@users.noreply.github.com>
2018-06-29 14:24:50 +0200

    lda and graphs

--- load_data.py
+++ load_data.py
@@ -1,4 +1,4 @@
-# Class to load data from a files
+# Class to load and save data
 import cv2
 import numpy as np
 import pandas as pd
@@ -10,6 +10,7 @@ class LoadData:
 		self.numbers_of_folders = numbers_of_folders
 		self.data_dir = r'C:\\Users\\asus\\Desktop\\data_science_python\\ph_project\\ph_datasets\\'
 		self.images_list = []
+
 	def load_data(self):
 		for j in range(self.numbers_of_folders):
 			for i in range(self.files):
@@ -18,6 +19,15 @@ class LoadData:
 					self.images_list.append([image[3][3][0],image[3][3][1],image[3][3][2],i])
 				except Exception:
 					print('no image')
-		df = pd.DataFrame(np.asarray(self.images_list), columns=['blue','green','red','label'])
-		return df 
-	 
+		self.df = pd.DataFrame(np.asarray(self.images_list), columns=['blue', 'green', 'red', 'label'])
+		return self.df
+
+	def save_data(self, filename):
+		if len(self.df.index) != 0:
+			print("Save data")
+			self.df.to_csv(filename, index=False)
+
+	def load_from_file(self, filename):
+		self.df = pd.read_csv(filename)
+		return self.df
+	
