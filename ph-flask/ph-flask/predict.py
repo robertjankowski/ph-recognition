17385325a7e16d68bc691d0c72343dfbb55e723e
robertjankowski <robjanw11@wp.pl>
2018-08-19 17:01:20 +0200

    attempt to result page

--- ph-flask/ph-flask/predict.py
+++ ph-flask/ph-flask/predict.py
@@ -10,9 +10,9 @@ bp = Blueprint('predict', __name__, url_prefix='/predict')
 @bp.route('/', methods=('GET', 'POST'))
 def predict():
     if request.method == 'POST':
-        blue = request.form['blue']
-        red = request.form['red']
-        green = request.form['green']
+        blue = int(request.form['blue'])
+        red = int(request.form['red'])
+        green = int(request.form['green'])
 
         error = None
 
@@ -23,8 +23,24 @@ def predict():
         elif not green:
             error = 'Green color is required'
 
+        if blue < 0 and blue > 255:
+            error = 'Range 0 to 255'
+        if red < 0 and red > 255:
+            error = 'Range 0 to 255'
+        if green < 0 and red > 255:
+            error = 'Range 0 to 255'
+        
+        # TODO 
+        # return render_template('result.html')
+        return redirect('predict/result')
+
+        flash(error)
+
+        
         # TODO
         # 1. complete predicting
         # 2. return new view
 
     return render_template('predict.html')
+
+
